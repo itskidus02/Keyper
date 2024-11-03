cb1ba0e35e3f47cb62b07effc35ee738d268e49f
Kidus Asebe <kidusasebe2019@gmail.com>
2024-11-03 11:50:00 +0300

    .

--- client/src/components/VaultHeader.jsx
+++ client/src/components/VaultHeader.jsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export function VaultHeader({ name, createdAt }) {
   return (
-    <Card className="mb-2">
+    <Card className="-mb-5">
       <CardHeader>
         <CardTitle className="text-4xl uppercase">{name} vault</CardTitle>
       </CardHeader>
