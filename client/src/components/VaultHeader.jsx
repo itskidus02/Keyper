9532ccb70bff9ff9aebf5072192da05bdc7d1897
Kidus Asebe <kidusasebe2019@gmail.com>
2024-11-04 12:49:33 +0300

    header title

--- client/src/components/VaultHeader.jsx
+++ client/src/components/VaultHeader.jsx
@@ -3,10 +3,12 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 export function VaultHeader({ name, createdAt }) {
   return (
     <Card className="-mb-5">
-      <CardHeader>
-        <CardTitle className="text-4xl text-muted-foreground uppercase">{name} vault</CardTitle>
+      <CardHeader className="flex flex-row items-center gap-2">
+        <CardTitle className="text-4xl uppercase whitespace-nowrap">{name}</CardTitle>
+        <CardTitle className="text-[#71717a] text-2xl uppercase whitespace-nowrap">
+          vault
+        </CardTitle>
       </CardHeader>
-   
     </Card>
   );
 }
