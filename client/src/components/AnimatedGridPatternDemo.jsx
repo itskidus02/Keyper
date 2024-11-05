93c2d213169f3077b389d9be2e1bdd7780ca5446
Kidus Asebe <kidusasebe2019@gmail.com>
2024-11-05 14:20:56 +0300

    hero buttons

--- client/src/components/AnimatedGridPatternDemo.jsx
+++ client/src/components/AnimatedGridPatternDemo.jsx
@@ -7,7 +7,7 @@ export function AnimatedGridPatternDemo() {
     <div className="relative flex justify-center h-screen w-full p-2">
       <div className="z-10 whitespace-pre-wrap mt-[10rem] text-center text-5xl font-medium tracking-tighter text-black dark:text-white">
         <h1 className="text-4xl md:text-6xl lg:text-9xl font-bold tracking-tight animate-in fade-in slide-in-from-bottom-8 duration-700">
-          <span className="text-primary">Secure.</span>{" "}
+          <span className="text-primary ">Secure.</span>{" "}
           <span className="text-primary">Simple.</span>{" "}
           <span className="bg-gradient-to-r from-blue-600 to-cyan-600 bg-clip-text text-transparent">
             Safe.
@@ -18,16 +18,16 @@ export function AnimatedGridPatternDemo() {
           effortless password management for ultimate peace of mind.
         </p>
         <div className="flex flex-col justify-center mt-[2rem] sm:flex-row gap-4 w-full sm:w-auto animate-in fade-in slide-in-from-bottom-16 duration-700">
-          <Button
+          <Button variant="gooeyLeft"
             size="lg"
-            className="w-full sm:w-auto bg-gradient-to-r from-blue-600 to-cyan-600 hover:from-blue-700 hover:to-cyan-700"
+            className="w-full sm:w-auto transition-all duration-300 text-white bg-gradient-to-r from-blue-600 to-cyan-600 "
           >
             Dashboard
           </Button>
           <Button
             size="lg"
-            variant="outline"
-            className="w-full sm:w-auto border-white/20 hover:bg-white/10"
+            variant="gooeyLeft"
+            className="w-full bg-white shadow-lg text-black sm:w-auto border "
           >
             See How It Works
           </Button>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,15 @@ export default {
   ],
   theme: {
   	extend: {
+		keyframes: {
+			"shine": {
+			  from: { backgroundPosition: '200% 0' },
+			  to: { backgroundPosition: '-200% 0' },
+			},
+		  },
+		  animation: {
+			"shine": "shine 8s ease-in-out infinite",
+		  },
 		fontFamily: {
 		
 			poppins: ['Poppins', 'sans-serif'],
