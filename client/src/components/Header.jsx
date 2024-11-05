2664ac0089f2ac419afeabf1aa99c2e918f37902
Kidus Asebe <kidusasebe2019@gmail.com>
2024-11-05 10:47:37 +0300

    header logo change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,20 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { ThemeToggle } from './theme-toggle';
+import { useTheme } from 'next-themes';
 import logo from "../assets/images/logo.png";
+import wlogo from "../assets/images/wlogo.png";
+
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  
+  const { resolvedTheme } = useTheme();
+  const currentLogo = resolvedTheme === "dark" ? wlogo : logo;  
   return (
     <header className='sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
       <div className='flex h-14 items-center justify-between max-w-6xl mx-auto px-4'>
         <Link className="flex items-center gap-3" to='/'>
-          <img src={logo} className='w-10 h-8' alt="LockBox Logo" />
+          <img src={currentLogo} className='w-10 h-8' alt="LockBox Logo" />
           <h1 className='font-bold'>LockBox</h1>
         </Link>
         
