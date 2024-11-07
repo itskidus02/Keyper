1fed3f1fe986d3d947d1ed7e6464fd81a16a2d5b
Kidus Asebe <kidusasebe2019@gmail.com>
2024-11-07 12:32:40 +0300

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8080',
+        target: 'http://localhost:8080',
         secure: false,
         changeOrigin: true
       },
