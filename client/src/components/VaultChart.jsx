d860778388afbff5960520a287cbb522b2f36442
Kidus Asebe <kidusasebe2019@gmail.com>
2024-11-10 13:14:47 +0300

    .

--- client/src/components/VaultChart.jsx
+++ client/src/components/VaultChart.jsx
@@ -75,8 +75,8 @@ export function VaultChart() {
             <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
             <defs>
               <linearGradient id="fillVaults" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="var(--color-vault)" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="var(--color-vault)" stopOpacity={0.1} />
+                <stop offset="5%" stopColor="var(--color-vaultCount)" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="var(--color-vaultCount)" stopOpacity={0.1} />
               </linearGradient>
             </defs>
             <Area
